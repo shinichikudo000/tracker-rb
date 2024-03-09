1a8edee09d1fef07a2355d505eac7589bfb8022a
acostayuukichi <acostayuukichi@gmail.com>
2024-03-09 14:44:13 +0800

    fix the date issues in calendar

--- front-end/components/ui/AddNewTaskForm.tsx
+++ front-end/components/ui/AddNewTaskForm.tsx
@@ -162,7 +162,7 @@ export default function AddNewTaskForm({category} : {category?: CategoryType}) {
                                             selected={field.value}
                                             onSelect={field.onChange}
                                             disabled={(date) =>
-                                            date > new Date() || date < new Date("1900-01-01")
+                                            date < new Date()
                                             }
                                             initialFocus
                                         />
